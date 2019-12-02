b2e9a7800554ab104c4146a8aa6ec07e87a93874
Meilunzhi <meilunzhi@gmail.com>
2019-12-02 11:33:44 +0800

    add

--- scripts/site-types/loupan_v3.sh
+++ scripts/site-types/loupan_v3.sh
@@ -47,7 +47,7 @@ block="server {
 
     charset utf-8;
 
-    include "/home/vagrant/code/nginx-configure/rewrite/rewrite_loupan_v2.com.conf";
+    include "/home/vagrant/code/nginx-configure/rewrite/rewrite_loupan_v3.com.conf";
 
     $rewritesTXT
 
